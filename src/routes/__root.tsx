b3f223664d6aa5857011b90453ac928db2140dc1
VinewZ <vinewalves@hotmail.com>
2026-02-06 12:55:38 -0300

    setup OBR ready event

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,29 +1,34 @@
-// import { TanStackDevtools } from "@tanstack/react-devtools";
+import OBR from "@owlbear-rodeo/sdk";
+import { TanStackDevtools } from "@tanstack/react-devtools";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-// import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+// IDK why, but if I remove this, no other OBR.onReady() will work, even in other files
+OBR.onReady(() => {
+  console.log("OBR is ready, RENDERING PAGE");
+});
+
 export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
-      {
-        // <TanStackDevtools
-        //   config={{
-        //     position: "bottom-right",
-        //   }}
-        //   plugins={[
-        //     {
-        //       name: "Tanstack Router",
-        //       render: <TanStackRouterDevtoolsPanel />,
-        //     },
-        //   ]}
-        // />
-      }
+      <TanStackDevtools
+        config={{
+          position: "bottom-right",
+          triggerHidden: true,
+        }}
+        plugins={[
+          {
+            name: "Tanstack Router",
+            render: <TanStackRouterDevtoolsPanel />,
+          },
+        ]}
+      />
     </>
   ),
 });
